a9732c0610cfcff2468795f5e591d72a3270bf34
eriko7878 <135241136+eriko7878@users.noreply.github.com>
2023-08-02 20:33:44 +0900

    kadai_005ディレクトリにbmi.jsをアップロード

---
+++ kdai_005/bmi.js
@@ -0,0 +1,28 @@
+//変数の宣言
+let userName;
+
+//値の代入
+userName = '体重(kg)';
+
+//変数の宣言・値の代入
+let userNumber = 68;
+
+//コンソールへの出力
+console.log(userName);
+console.log(userNumber);
+
+//値の再代入
+userName ='身長(m)';
+userNumber ='1.7';
+
+//コンソールへの出力
+console.log(userName);
+console.log(userNumber);
+
+//値の再代入
+userName ='BMI';
+userNumber =68 / 1.7 * 1.7;
+
+//コンソールへの出力
+console.log(userName);
+console.log(userNumber);
